b13275075b6908fb5820879fc4daca857f6e2ad8
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2023-08-04 23:19:57 +0200

    fix(test): change test assertion in test_generated_data.py
    change equality test to test on difference. Number format led to inequality
    (0.50000000...59 compared to 0.5)

--- tests/test_generated_data.py
+++ tests/test_generated_data.py
@@ -5,6 +5,8 @@ Created on Fri Aug  4 12:45:15 2023
 @author: peruzzetto
 """
 
+import numpy as np
+
 import tilupy.make_topo
 
 
@@ -12,4 +14,4 @@ def test_topo_gray():
 
     x, y, z = tilupy.make_topo.gray99()
 
-    assert (y[-1] == 0.5) & (len(x) == 361)
+    assert (np.abs(y[-1] - 0.5) < 1e-6) & (len(x) == 361)
