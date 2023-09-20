1af2a4aec3c63e0663c45fb349d8b423d2cee2b6
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2023-09-20 23:42:34 +0200

    fix(calibration): fix old version of module import not corrected
    change dem to raster in module import

--- src/tilupy/calibration.py
+++ src/tilupy/calibration.py
@@ -10,51 +10,54 @@ import pandas as pd
 
 from tilupy import read
 from tilupy import utils
-from tilupy import dem
+from tilupy import raster
+
 
 def CSI(simu, observation=None, h_threshs=[1], state='h_final'):
-    
-    res=[]
+
+    res = []
     assert(observation is not None)
-    
+
     if isinstance(observation, str):
-        _, _, observation, _ = dem.read_raster(observation)
-        
+        _, _, observation, _ = raster.read_raster(observation)
+
     strs = state.split('_')
     if state == 'h_max':
         d = simu.h_max
     else:
         d = simu.get_static_output(strs[0], strs[1]).d
     for h_thresh in h_threshs:
-        array = 1*d>h_thresh
+        array = 1*d > h_thresh
         res.append(utils.CSI(array, observation))
-        
+
     return h_threshs, res
-        
+
+
 def diff_runout(simu, point=None, h_threshs=[1],
                 section=None, orientation='W-E',
                 state='h_max',
                 get_contour_kws=None):
-    
+
     res = []
     assert(point is not None)
-    
+
     if get_contour_kws is None:
         get_contour_kws = dict()
-        
+
     if state == 'h_max':
-        d=simu.h_max
+        d = simu.h_max
     else:
         strs = state.split('_')
         d = simu.get_static_output(strs[0], strs[1]).d
-    xc, yc = utils.get_contour(simu.x,simu.y,d,h_threshs, **get_contour_kws)
-    
+    xc, yc = utils.get_contour(simu.x, simu.y, d, h_threshs, **get_contour_kws)
+
     for h in h_threshs:
         res.append(utils.diff_runout(xc[h], yc[h], point, section=section,
                                      orientation=orientation))
-        
+
     return h_threshs, res
-    
+
+
 def eval_simus(simus, methods, calib_parameters, methods_kws,
                code='shaltop',
                recorded_params=['delta1'],
@@ -80,16 +83,17 @@ def eval_simus(simus, methods, calib_parameters, methods_kws,
         methods_kws = [methods_kws]
 
     if isinstance(simus, dict):
-        simus=pd.DataFrame(simus)
+        simus = pd.DataFrame(simus)
     if isinstance(simus, pd.DataFrame):
         simus_list = []
         for i in range(simus.shape[0]):
-            simus_list.append(read.get_results(code, **simus.iloc[i, :].to_dict()))
+            simus_list.append(read.get_results(
+                code, **simus.iloc[i, :].to_dict()))
         simus2 = simus.copy()
     else:
         simus_list = simus
         simus2 = pd.DataFrame()
-    
+
     for param in recorded_params:
         simus2[param] = np.nan
     fn = dict()
@@ -97,13 +101,13 @@ def eval_simus(simus, methods, calib_parameters, methods_kws,
         fn[method] = globals()[method]
         simus2[method] = np.nan
     simus2[calib_parameter_name] = np.nan
-    
+
     ns = len(simus_list)
     nc = len(calib_parameters)
     nn = ns*nc
-        
-    res = pd.DataFrame(columns=simus2.columns, index=np.arange(nn))    
-        
+
+    res = pd.DataFrame(columns=simus2.columns, index=np.arange(nn))
+
     for i, simu in enumerate(simus_list):
         # Initiate fields
         istart = i*nc
@@ -117,12 +121,5 @@ def eval_simus(simus, methods, calib_parameters, methods_kws,
                 res.loc[:, param].iloc[istart:iend] = simu.params[param]
             res.loc[:, calib_parameter_name].iloc[istart:iend] = calib_parameters
             res.loc[:, method].iloc[istart:iend] = calib_res
-            
+
     return res
-
-
-            
-    
-    
-        
-    
