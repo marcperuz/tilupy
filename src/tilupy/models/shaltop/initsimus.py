031a5a9af9d3e7b9f92af81b9bb66e826a05ba98
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-01-11 18:08:09 +0100

    feat(shaltop): add function to generate slurm job/conf files
    Add function to generate slurm job/conf files to run multiple simulations
    on a cluster.

--- src/tilupy/models/shaltop/initsimus.py
+++ src/tilupy/models/shaltop/initsimus.py
@@ -7,24 +7,23 @@ Created on Tue May 25 15:18:31 2021
 """
 
 import os
+import posixpath
 import numpy as np
+
+from tilupy.utils import format_path_linux
 
 import tilupy.notations
 import tilupy.raster
 
-README_PARAM_MATCH = dict(tmax='tmax',
-                          CFL='cflhyp',
-                          h_min='eps0',
-                          dt_im_output='dt_im')
 
-SHALTOP_LAW_ID = dict(coulomb=1,
-                      voellmy=8,
-                      bingham=6,
-                      muI=7)
+README_PARAM_MATCH = dict(
+    tmax="tmax", CFL="cflhyp", h_min="eps0", dt_im_output="dt_im"
+)
+
+SHALTOP_LAW_ID = dict(coulomb=1, voellmy=8, bingham=6, muI=7)
 
 
-def write_params_file(params, directory=None,
-                      file_name='params.txt'):
+def write_params_file(params, directory=None, file_name="params.txt"):
     """
     Write params file for shaltop simulations
 
@@ -47,31 +46,143 @@ def write_params_file(params, directory=None,
 
     if directory is None:
         directory = os.getcwd()
-    with open(os.path.join(directory, file_name), 'w') as file_params:
+    with open(os.path.join(directory, file_name), "w") as file_params:
         for name in params:
             val = params[name]
             if isinstance(val, int) or isinstance(val, np.int64):
-                file_params.write('{:s} {:d}\n'.format(name, val))
+                file_params.write("{:s} {:d}\n".format(name, val))
             if isinstance(val, float) or isinstance(val, np.float64):
-                file_params.write('{:s} {:.8G}\n'.format(name, val))
+                file_params.write("{:s} {:.8G}\n".format(name, val))
             if isinstance(val, str):
-                file_params.write('{:s} {:s}\n'.format(name, val))
+                file_params.write("{:s} {:s}\n".format(name, val))
 
 
 def raster_to_shaltop_txtfile(file_in, file_out, folder_out=None):
-
     if folder_out is not None:
         file_out = os.path.join(folder_out, file_out)
 
     x, y, rast = tilupy.raster.read_raster(file_in)
-    np.savetxt(file_out,
-               np.reshape(np.flip(rast, axis=0), (rast.size, 1)),
-               fmt='%.12G')
+    np.savetxt(
+        file_out,
+        np.reshape(np.flip(rast, axis=0), (rast.size, 1)),
+        fmt="%.12G",
+    )
 
-    res = dict(x0=x[0], y0=y[0], dx=x[1]-x[0], dy=y[1]-y[0],
-               nx=len(x), ny=len(y))
+    res = dict(
+        x0=x[0], y0=y[0], dx=x[1] - x[0], dy=y[1] - y[0], nx=len(x), ny=len(y)
+    )
 
     return res
+
+
+def write_job_files(
+    dirs,
+    param_files,
+    file_job,
+    job_name,
+    max_time_hours=24,
+    ncores_per_node=6,
+    partitions="cpuall,data,datanew",
+    shaltop_file="shaltop",
+    folder_conf_in_job=None,
+    replace_path=None,
+    number_conf_file=True,
+):
+    """
+    Write job/conf files for slurm jobs. The conf contains all the commands
+    needed to run each simulation (one command per simulation).
+
+    Parameters
+    ----------
+    dirs : list of string
+        list of paths where simus will be run.
+    param_files : list string
+        list of shaltop parameter files.
+    file_job : string
+        name of job file called by sbatch.
+    job_name : string
+        name of conf file used by file_job.
+    max_time_hours : int, optional
+        Maximum job duration in hours before stop. The default is 24.
+    ncores_per_node : int, optional
+        Number of cores per nodes. Used to know the number of nodes required
+        for the job. The default is 6.
+    partitions : string, optional
+        Names of partitions on which jobs can be launched.
+        The default is "cpuall,data,datanew".
+    shaltop_file : string, optional
+        Bash command used to call shaltop. Can be a path.
+        The default is "shaltop".
+    folder_conf_in_job : string, optional
+        Folder where the conf file is located. The default is the folder
+        path of file_job.
+    replace_path : list, optional
+        replace replace_path[0] by replace_path[1] for every path in dir. This
+        is used if simulations are prepared and run on two different machines
+        (e.g. laptop and cluster).
+        The default is None.
+    number_conf_file : bool, optional
+        If True, add a number in front of each line of the conf file. Required
+        to identify slurm jobs.
+        The default is True.
+
+    Returns
+    -------
+    None.
+
+    """
+    ntasks = len(dirs)
+    nnodes = int(np.ceil(ntasks / ncores_per_node))
+
+    if folder_conf_in_job is None:
+        folder_conf_in_job = os.path.dirname(file_job)
+        if folder_conf_in_job == "":
+            folder_conf_in_job = "."
+
+    with open(file_job + ".conf", "w", newline="\n") as conf_file:
+        if number_conf_file:
+            line = "{:d} {:s} {:s} {:s}\n"
+        else:
+            line = "{:s} {:s} {:s}\n"
+        for i in range(ntasks):
+            if replace_path is not None:
+                folder = dirs[i].replace(replace_path[0], replace_path[1])
+                param_file = param_files[i].replace(
+                    replace_path[0], replace_path[1]
+                )
+            else:
+                folder = dirs[i]
+                param_file = param_files[i]
+            folder = format_path_linux(folder)
+            param_file = format_path_linux(param_file)
+            if number_conf_file:
+                line2 = line.format(i, shaltop_file, folder, param_file)
+            else:
+                line2 = line.format(shaltop_file, folder, param_file)
+            conf_file.write(line2)
+
+    n_hours = np.floor(max_time_hours)
+    n_min = (max_time_hours - n_hours) * 60
+    str_time = "{:02.0f}:{:02.0f}:00\n".format(n_hours, n_min)
+
+    basename = os.path.basename(file_job)
+    path_conf_in_job = posixpath.join(folder_conf_in_job, basename + ".conf")
+
+    with open(file_job + ".job", "w", newline="\n") as job_file:
+        job_file.write("#!/bin/sh\n")
+        job_file.write("#SBATCH -J multijob\n")
+        job_file.write("#SBATCH --job-name={:s}\n".format(job_name))
+        job_file.write("#SBATCH --output={:s}%j.out\n".format(job_name))
+        job_file.write("#SBATCH --partition " + partitions + "\n")
+        job_file.write("#SBATCH --nodes={:d}".format(nnodes) + "\n")
+        job_file.write("#SBATCH --ntasks={:d}".format(ntasks) + "\n")
+        job_file.write("#SBATCH --time={:s}\n".format(str_time))
+        job_file.write("\n")
+        job_file.write("module purge\n")
+        job_file.write("module load slurm\n")
+        job_file.write("\n")
+        line = "srun -n {:d} -l --multi-prog {:s}"
+        job_file.write(line.format(ntasks, path_conf_in_job))
 
 
 def make_simus(law, rheol_params, folder_data, folder_out, readme_file):
@@ -93,57 +204,58 @@ def make_simus(law, rheol_params, folder_data, folder_out, readme_file):
 
     """
     # Get topography and initial mass, and write them in Shaltop format
-    zfile = os.path.join(folder_data, 'topo.asc')
-    mfile = os.path.join(folder_data, 'mass.asc')
+    zfile = os.path.join(folder_data, "topo.asc")
+    mfile = os.path.join(folder_data, "mass.asc")
     x, y, z, dx = tilupy.raster.read_ascii(zfile)
     _, _, m, _ = tilupy.raster.read_ascii(mfile)
-    np.savetxt(os.path.join(folder_out, 'z.d'), z.T.flatten())
-    np.savetxt(os.path.join(folder_out, 'm.d'), m.T.flatten())
+    np.savetxt(os.path.join(folder_out, "z.d"), z.T.flatten())
+    np.savetxt(os.path.join(folder_out, "m.d"), m.T.flatten())
 
     # Get simulation parameters from README.txt and raster .asc files
     params = tilupy.notations.readme_to_params(readme_file, README_PARAM_MATCH)
-    params['nx'] = len(x)
-    params['ny'] = len(y)
-    params['per'] = dx*len(x)
-    params['pery'] = dx*len(y)
-    params['file_m_init'] = '../m.d'
-    params['file_z_init'] = '../z.d'
+    params["nx"] = len(x)
+    params["ny"] = len(y)
+    params["per"] = dx * len(x)
+    params["pery"] = dx * len(y)
+    params["file_m_init"] = "../m.d"
+    params["file_z_init"] = "../z.d"
 
     # Folder for rheological law, and set params accordingly
     folder_law = os.path.join(folder_out, law)
-    params['icomp'] = SHALTOP_LAW_ID[law]
+    params["icomp"] = SHALTOP_LAW_ID[law]
 
     param_names = [param for param in rheol_params]
 
     texts = tilupy.notations.make_rheol_string(rheol_params, law)
 
     # Run shaltop file
-    run_shaltop_file = os.path.join(folder_law, 'run_shaltop.sh')
+    run_shaltop_file = os.path.join(folder_law, "run_shaltop.sh")
     file_txt = ""
 
     for i in range(len(rheol_params[param_names[0]])):
-
         simu_text = texts[i]
         for param_name in param_names:
             params[param_name] = rheol_params[param_name][i]
-        params['folder_output'] = simu_text
+        params["folder_output"] = simu_text
         folder_results = os.path.join(folder_law, simu_text)
         os.makedirs(folder_results, exist_ok=True)
-        with open(os.path.join(folder_results, '.gitignore'), 'w') as fid:
-            fid.write('# Ignore everything in this directory')
-            fid.write('*')
-            fid.write('# Except this file')
-            fid.write('!.gitignore')
+        with open(os.path.join(folder_results, ".gitignore"), "w") as fid:
+            fid.write("# Ignore everything in this directory")
+            fid.write("*")
+            fid.write("# Except this file")
+            fid.write("!.gitignore")
 
-        write_params_file(params, directory=folder_law,
-                          file_name=simu_text+'.txt')
-        file_txt += 'start_time=`date +%s`\n'
-        file_txt += 'shaltop "" ' + simu_text + '.txt\n'
-        file_txt += 'end_time=`date +%s`\n'
-        file_txt += 'elapsed_time=$(($end_time - $start_time))\n'
-        file_txt += ('string_time="${start_time} ' +
-                     simu_text + ' ${elapsed_time}"\n')
-        file_txt += 'echo ${string_time} >> simulation_duration.txt\n\n'
+        write_params_file(
+            params, directory=folder_law, file_name=simu_text + ".txt"
+        )
+        file_txt += "start_time=`date +%s`\n"
+        file_txt += 'shaltop "" ' + simu_text + ".txt\n"
+        file_txt += "end_time=`date +%s`\n"
+        file_txt += "elapsed_time=$(($end_time - $start_time))\n"
+        file_txt += (
+            'string_time="${start_time} ' + simu_text + ' ${elapsed_time}"\n'
+        )
+        file_txt += "echo ${string_time} >> simulation_duration.txt\n\n"
 
     with open(run_shaltop_file, "w") as fid:
         fid.write(file_txt)
--- src/tilupy/utils.py
+++ src/tilupy/utils.py
@@ -11,45 +11,47 @@ import matplotlib.pyplot as plt
 import shapely.geometry as geom
 import shapely.ops
 
-def CSI(pred, obs):
-    
-    ipred = pred>0
-    iobs = obs>0
-    
-    TP = np.sum(ipred*iobs)
-    FP = np.sum(ipred*~iobs)
-    FN = np.sum(~ipred*iobs)
-    
-    return TP/(TP+FP+FN)
 
-def diff_runout(x_contour, y_contour, point_ref, section=None,
-                orientation='W-E'):
-    
+def CSI(pred, obs):
+    ipred = pred > 0
+    iobs = obs > 0
+
+    TP = np.sum(ipred * iobs)
+    FP = np.sum(ipred * ~iobs)
+    FN = np.sum(~ipred * iobs)
+
+    return TP / (TP + FP + FN)
+
+
+def diff_runout(
+    x_contour, y_contour, point_ref, section=None, orientation="W-E"
+):
     npts = len(x_contour)
-    contour = geom.LineString([(x_contour[i],y_contour[i])
-                               for i in range(npts)])
+    contour = geom.LineString(
+        [(x_contour[i], y_contour[i]) for i in range(npts)]
+    )
     point = geom.Point(point_ref)
     if section is None:
         return point.distance(contour)
     elif isinstance(section, np.ndarray):
         section = geom.LineString(section)
-        
-    assert(isinstance(section, geom.LineString))
+
+    assert isinstance(section, geom.LineString)
     section = revert_line(section, orientation)
     intersections = section.intersection(contour)
     if isinstance(intersections, geom.MultiPoint):
         intersections = geom.LineString(section.intersection(contour))
     intersections = np.array(intersections.coords)
-    if orientation == 'W-E':
-        i = np.argmax(intersections[:,0])
-    if orientation == 'E-W':
-        i = np.argmin(intersections[:,0])
-    if orientation == 'S-N':
-        i = np.argmax(intersections[:,1])
-    if orientation == 'N-S':
-        i = np.argmin(intersections[:,1])
-    intersection = geom.Point(intersections[i,:])
-    
+    if orientation == "W-E":
+        i = np.argmax(intersections[:, 0])
+    if orientation == "E-W":
+        i = np.argmin(intersections[:, 0])
+    if orientation == "S-N":
+        i = np.argmax(intersections[:, 1])
+    if orientation == "N-S":
+        i = np.argmin(intersections[:, 1])
+    intersection = geom.Point(intersections[i, :])
+
     #######
     # plt.figure()
     # cont = np.array(contour.coords)
@@ -61,71 +63,97 @@ def diff_runout(x_contour, y_contour, point_ref, section=None,
     #           pt[:,0], pt[:,1], 'o',
     #           inter[:,0], inter[:,1],'x')
     #######
-    
+
     return section.project(intersection) - section.project(point)
-    
-def revert_line(line, orientation='W-E'):
-    
+
+
+def revert_line(line, orientation="W-E"):
     pt_init = line.coords[0]
     pt_end = line.coords[-1]
-    if orientation == 'W-E':
-        if pt_init[0]>pt_end[0]:
+    if orientation == "W-E":
+        if pt_init[0] > pt_end[0]:
             line = shapely.ops.substring(line, 1, 0, normalized=True)
-    elif orientation == 'E-W':
-        if pt_init[0]<pt_end[0]:
+    elif orientation == "E-W":
+        if pt_init[0] < pt_end[0]:
             line = shapely.ops.substring(line, 1, 0, normalized=True)
-    if orientation == 'S-N':
-        if pt_init[1]>pt_end[1]:
+    if orientation == "S-N":
+        if pt_init[1] > pt_end[1]:
             line = shapely.ops.substring(line, 1, 0, normalized=True)
-    elif orientation == 'N-S':
-        if pt_init[1]<pt_end[1]:
+    elif orientation == "N-S":
+        if pt_init[1] < pt_end[1]:
             line = shapely.ops.substring(line, 1, 0, normalized=True)
-    
+
     return line
 
-def get_contour(x,y,z,zlevels,indstep=1,maxdist=30, closed_contour=True):
-    
-    #Add sup_value at the border of the array, to make sure contour
-    #lines are closed
+
+def get_contour(x, y, z, zlevels, indstep=1, maxdist=30, closed_contour=True):
+    # Add sup_value at the border of the array, to make sure contour
+    # lines are closed
     if closed_contour:
         z2 = z.copy()
         ni = z2.shape[0]
         nj = z2.shape[1]
         z2 = np.vstack([np.zeros((1, nj)), z2, np.zeros((1, nj))])
-        z2 = np.hstack([np.zeros((ni+2, 1)), z2, np.zeros((ni+2, 1))])
-        dxi = x[1]-x[0]
-        dxf = x[-1]-x[-2]
+        z2 = np.hstack([np.zeros((ni + 2, 1)), z2, np.zeros((ni + 2, 1))])
+        dxi = x[1] - x[0]
+        dxf = x[-1] - x[-2]
         dyi = y[1] - y[0]
         dyf = y[-1] - y[-2]
-        x2 = np.insert(np.append(x, x[-1]+dxf), 0, x[0]-dxi)
-        y2 = np.insert(np.append(y, y[-1]+dyf), 0, y[0]-dyi)
+        x2 = np.insert(np.append(x, x[-1] + dxf), 0, x[0] - dxi)
+        y2 = np.insert(np.append(y, y[-1] + dyf), 0, y[0] - dyi)
     else:
         x2, y2, z2 = x, y, z
-    
+
     backend = plt.get_backend()
-    plt.switch_backend('Agg')
+    plt.switch_backend("Agg")
     plt.figure()
-    ax=plt.gca()    
-    cs=ax.contour(x2, y2, np.flip(z2, 0), zlevels)
-    nn1=1
-    v1=np.zeros((1,2))
-    xcontour={}
-    ycontour={}
+    ax = plt.gca()
+    cs = ax.contour(x2, y2, np.flip(z2, 0), zlevels)
+    nn1 = 1
+    v1 = np.zeros((1, 2))
+    xcontour = {}
+    ycontour = {}
     for indlevel in range(len(zlevels)):
         for p in cs.collections[indlevel].get_paths():
-            if (p.vertices.shape[0]>nn1):
-                v1=p.vertices
-                nn1=p.vertices.shape[0]
-        xc = [v1[::indstep,0]]
-        yc = [v1[::indstep,1]]
+            if p.vertices.shape[0] > nn1:
+                v1 = p.vertices
+                nn1 = p.vertices.shape[0]
+        xc = [v1[::indstep, 0]]
+        yc = [v1[::indstep, 1]]
         if maxdist is not None and not closed_contour:
-            ddx=np.abs(v1[0,0]-v1[-1,0])
-            ddy=np.abs(v1[0,1]-v1[-1,1])
-            dd=np.sqrt(ddx**2+ddy**2)
-            if (dd>maxdist) :
-                xc[0]=None
-                yc[0]=None
-        xcontour[zlevels[indlevel]]=xc[0]
-        ycontour[zlevels[indlevel]]=yc[0]
+            ddx = np.abs(v1[0, 0] - v1[-1, 0])
+            ddy = np.abs(v1[0, 1] - v1[-1, 1])
+            dd = np.sqrt(ddx**2 + ddy**2)
+            if dd > maxdist:
+                xc[0] = None
+                yc[0] = None
+        xcontour[zlevels[indlevel]] = xc[0]
+        ycontour[zlevels[indlevel]] = yc[0]
     plt.switch_backend(backend)
-    return xcontour,ycontour
+    return xcontour, ycontour
+
+
+def format_path_linux(path):
+    """
+    Change a Windows-type path to a path formatted for Linux. \\ are changed
+    to /, and partitions like "C:" are changed to "/mnt/c/"
+
+    Parameters
+    ----------
+    path : string
+        String with the path to be modified.
+
+    Returns
+    -------
+    path2 : string
+        Formatted path.
+
+    """
+    if path[1] == ":":
+        path2 = "/mnt/{:s}/".format(path[0].lower()) + path[2:]
+    else:
+        path2 = path
+    path2 = path2.replace("\\", "/")
+    if " " in path2:
+        path2 = '"' + path2 + '"'
+    return path2
