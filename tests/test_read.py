5e01f1394724befce982075cef8b4f8ae907375b
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-01-16 15:58:01 +0100

    test(read): add fixture with TemporalResults exmaple
    Add fixture with 3D Tporalresults example (2D spatial + time). Spatial
    data is genreated with a multivariate normal law, will be used to test
    1D spatial integration functions (process and plot)

--- tests/test_read.py
+++ tests/test_read.py
@@ -27,6 +27,51 @@ def simple_temporal_results():
     return test_data
 
 
+@pytest.fixture
+def gaussian_temporal_results():
+    xmin, xmax = -1, 1
+    ymin, ymax = -1, 1
+    dx = dy = 0.1
+    x = np.arange(xmin, xmax + dx / 2, dx)
+    y = np.arange(ymin, ymax + dy / 2, dy)
+
+    nx = len(x)
+    ny = len(y)
+    nt = 3
+
+    step_x = (xmax - xmin) / 5
+    step_y = (ymax - ymin) / 8
+    xv, yv = np.meshgrid(x, y, indexing="xy")
+    yv = np.flip(yv, axis=0)
+    pos = np.dstack((xv, yv))
+    h = np.zeros((ny, nx, nt))
+    meanx = [xmin + step_x, xmin + 1.5 * step_x, xmin + 2.5 * step_x]
+    meany = [
+        (ymax + ymin) / 2 - step_y,
+        (ymax + ymin) / 2,
+        (ymax + ymin) / 2 + step_y,
+    ]
+    stdx = [step_x / 4, step_x / 3, step_x / 2]
+    stdy = [step_y / 3, step_y / 3, step_y / 3]
+    cov_r = [0, 0.3, 0]
+    for i in range(nt):
+        mean = [meanx[i], meany[i]]
+        cov = cov_r[i] * np.sqrt(stdx[i] * stdy[i])
+        cov_m = [[stdx[i], cov], [cov, stdy[i]]]
+        rm = scipy.stats.multivariate_normal(mean, cov_m)
+        h[:, :, i] = rm.pdf(pos)
+
+    test_data = tiread.TemporalResults(
+        "h",
+        h,
+        [0, 1, 2],
+        x=x,
+        y=y,
+    )
+
+    return test_data
+
+
 @pytest.mark.parametrize(
     "arg, expected",
     [
