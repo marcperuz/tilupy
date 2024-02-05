bc641b88ad7cc516db73176c20f3cbe6bfab1e65
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-02-05 17:16:18 +0100

    fix(gray99): correct setup for gray99 experiment
    Correct the offset of calotte position for the initial mass. Style is also improved.

--- src/tilupy/initdata.py
+++ src/tilupy/initdata.py
@@ -13,44 +13,60 @@ import tilupy.make_mass
 import tilupy.plot
 
 
-def make_constant_slope(folder_out, theta=10, m_radius=50, m_height=50,
-                        m_x=None, m_y=None,
-                        xmax=800, ymax=550, cellsize=2.5):
+def make_constant_slope(
+    folder_out,
+    theta=10,
+    m_radius=50,
+    m_height=50,
+    m_x=None,
+    m_y=None,
+    xmax=800,
+    ymax=550,
+    cellsize=2.5,
+):
     """Init topography and mass for simulation on constant slope."""
     # Create mesh
-    x = np.linspace(0, xmax, int(np.round(xmax/cellsize)))
-    y = np.linspace(0, ymax, int(np.round(ymax/cellsize)))
+    x = np.linspace(0, xmax, int(np.round(xmax / cellsize)))
+    y = np.linspace(0, ymax, int(np.round(ymax / cellsize)))
     xmesh, ymesh = np.meshgrid(x, y)
 
     # Topography slope
     slope = np.tan(np.deg2rad(theta))
 
     # Topography array
-    z = -slope*(xmesh-xmax)
+    z = -slope * (xmesh - xmax)
 
     # Create initial mass
     if m_x is None:
-        m_x = 3*m_radius
+        m_x = 3 * m_radius
     if m_y is None:
-        m_y = ymax/2
-    m = 1-(xmesh - m_x)**2/m_radius**2 - (ymesh - m_y)**2/m_radius**2
-    m = np.maximum(m*m_height, 0)
+        m_y = ymax / 2
+    m = (
+        1
+        - (xmesh - m_x) ** 2 / m_radius**2
+        - (ymesh - m_y) ** 2 / m_radius**2
+    )
+    m = np.maximum(m * m_height, 0)
 
     # Write headers for ascii files
     nx = z.shape[1]
     ny = z.shape[0]
-    header_txt = 'ncols {:.0f}\nnrows {:.0f}\nxllcorner 0\nyllcorner 0\n'
-    header_txt += 'cellsize {:.4f}\nnodata_value -99999'
+    header_txt = "ncols {:.0f}\nnrows {:.0f}\nxllcorner 0\nyllcorner 0\n"
+    header_txt += "cellsize {:.4f}\nnodata_value -99999"
     header_txt = header_txt.format(nx, ny, cellsize)
 
-    for a, name in zip([z, m], ['topo', 'mass']):
-        np.savetxt(os.path.join(folder_out, name+'.asc'), a,
-                   header=header_txt,
-                   comments='')
+    for a, name in zip([z, m], ["topo", "mass"]):
+        np.savetxt(
+            os.path.join(folder_out, name + ".asc"),
+            a,
+            header=header_txt,
+            comments="",
+        )
 
 
-def gray99_topo_mass(dx=0.1, dy=0.1, save=False, folder_out=None):
-
+def gray99_topo_mass(
+    dx=0.1, dy=0.1, save=False, folder_out=None, res_type="true_normal"
+):
     # Initiate topography
     X, Y, Z = tilupy.make_topo.gray99(dx=dx, dy=dy)
 
@@ -59,23 +75,21 @@ def gray99_topo_mass(dx=0.1, dy=0.1, save=False, folder_out=None):
     # of 0.32 m (more precisely it is the length in the downslope direction)
     # The correspondig radius of the sphere, and the offset from the topography
     # in the topography normal direction (norm_offset) are deduced from these
-    # parameters
+    # parameters. See also Gig 3 in Wieland, Gray and Hutter (1999)
 
-    x0 = 0.06*np.cos(np.deg2rad(40))
+    x0 = 0.06 * np.cos(np.deg2rad(40))
     hmass = 0.22
     wmass = 0.32
-    radius = (wmass**2+hmass**2)/(2*hmass)
-    norm_offset = (hmass**2-wmass**2)/(2*hmass)
+    radius = (wmass**2 + hmass**2) / (2 * hmass)
+    norm_offset = (wmass**2 - hmass**2) / (2 * hmass)
     # Z = -np.tile(X, [len(Y), 1])*np.tan(np.deg2rad(20))
-    M = tilupy.make_mass.calotte(X, Y, Z, x0, 0,
-                                 radius, norm_offset=norm_offset,
-                                 res_type='true_normal')
+    M = tilupy.make_mass.calotte(
+        X, Y, Z, x0, 0, radius, norm_offset=norm_offset, res_type=res_type
+    )
 
     return X, Y, Z, M
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     x, y, z, m = gray99_topo_mass(dx=0.01, dy=0.01)
-    tilupy.plot.plot_data_on_topo(x, y, z, m,
-                                  topo_kwargs=dict(level_min=0.1))
+    tilupy.plot.plot_data_on_topo(x, y, z, m, topo_kwargs=dict(level_min=0.1))
--- src/tilupy/make_topo.py
+++ src/tilupy/make_topo.py
@@ -11,9 +11,22 @@ import scipy
 import tilupy.plot
 
 
-def gray99(nx=None, ny=None, xmin=-0.4, x1=1.75, x2=2.15,
-           xmax=3.2, ymax=0.5, R=1.1,
-           theta1=40, theta2=0, maxz=None, dx=0.01, dy=0.01, plot=False):
+def gray99(
+    nx=None,
+    ny=None,
+    xmin=-0.4,
+    x1=1.75,
+    x2=2.15,
+    xmax=3.2,
+    ymax=0.6,
+    R=1.1,
+    theta1=40,
+    theta2=0,
+    maxz=None,
+    dx=0.01,
+    dy=0.01,
+    plot=False,
+):
     """
     Construct channel as in Gray et all 99.
 
@@ -69,13 +82,13 @@ def gray99(nx=None, ny=None, xmin=-0.4, x1=1.75, x2=2.15,
     theta2 = np.deg2rad(theta2)
 
     if nx is None:
-        x = np.arange(xmin, xmax+dx/2, dx)
+        x = np.arange(xmin, xmax + dx / 2, dx)
         nx = len(x)
     else:
         x = np.linspace(xmin, xmax, nx)
 
     if ny is None:
-        y = np.arange(-ymax, ymax+dy/2, dy)
+        y = np.arange(-ymax, ymax + dy / 2, dy)
         ny = len(y)
     else:
         y = np.linspace(-ymax, ymax, ny)
@@ -85,42 +98,46 @@ def gray99(nx=None, ny=None, xmin=-0.4, x1=1.75, x2=2.15,
     # Superficial topography : channel
     # alpha=1/(2*R)*np.sin(0.5*np.pi*(x-x2)/(x1-x2))**2
     # alpha=1/(2*R)*np.abs(x-x2)**1/np.abs(x1-x2)**1
-    alpha = 1/(2*R)*(3*((x-x2)/(x1-x2))**2-2*((x-x2)/(x1-x2))**3)
+    alpha = (
+        1
+        / (2 * R)
+        * (3 * ((x - x2) / (x1 - x2)) ** 2 - 2 * ((x - x2) / (x1 - x2)) ** 3)
+    )
     alpha[x > x2] = 0
-    alpha[x < x1] = 1/(2*R)
+    alpha[x < x1] = 1 / (2 * R)
     alpha = np.tile(alpha.reshape((nx, 1)), (1, ny))
 
-    zchannel = alpha*np.abs(ycurv)**2
+    zchannel = alpha * np.abs(ycurv) ** 2
     # plt.figure()
     # plt.imshow(zchannel)
 
     # del alpha
 
     if not maxz:
-        maxz = R/2
+        maxz = R / 2
 
     zchannel[zchannel > maxz] = maxz
 
     # Base topography in curvilinear system.
     # The transition zone between x1 and x2 is a cylindre
-    zbase = -np.sin(theta2)*(x-xmax)
+    zbase = -np.sin(theta2) * (x - xmax)
 
     ind = (x <= x2) & (x >= x1)
-    angle = (x[ind]-x1)/(x2-x1)*(theta2-theta1)+theta1
-    R2 = (x2-x1)/(theta1-theta2)
-    z2 = -np.sin(theta2)*(x2-xmax)
-    zbase[ind] = R2*(1-np.cos(angle))-R2*(1-np.cos(theta2))+z2
+    angle = (x[ind] - x1) / (x2 - x1) * (theta2 - theta1) + theta1
+    R2 = (x2 - x1) / (theta1 - theta2)
+    z2 = -np.sin(theta2) * (x2 - xmax)
+    zbase[ind] = R2 * (1 - np.cos(angle)) - R2 * (1 - np.cos(theta2)) + z2
 
     ind = x <= x1
-    z1 = R2*(1-np.cos(theta1))-R2*(1-np.cos(theta2))+z2
-    zbase[ind] = -np.sin(theta1)*(x[ind]-x1)+z1
+    z1 = R2 * (1 - np.cos(theta1)) - R2 * (1 - np.cos(theta2)) + z2
+    zbase[ind] = -np.sin(theta1) * (x[ind] - x1) + z1
     zbase = np.tile(zbase.reshape((nx, 1)), (1, ny))
 
     # Conversion in fixed cartesian frame
-    zd = np.gradient(zbase, x[1]-x[0], edge_order=2, axis=0)
-    Xd = np.sqrt(1-zd**2)
+    zd = np.gradient(zbase, x[1] - x[0], edge_order=2, axis=0)
+    Xd = np.sqrt(1 - zd**2)
     X = scipy.integrate.cumtrapz(Xd, x, axis=0, initial=0)
-    X = X+xmin*np.cos(theta1)
+    X = X + xmin * np.cos(theta1)
 
     # plt.figure()
     # plt.plot(X[:,0])
@@ -130,27 +147,31 @@ def gray99(nx=None, ny=None, xmin=-0.4, x1=1.75, x2=2.15,
     # Topography conversion in fixed cartesian frame
     [Fx, Fy] = np.gradient(zbase, X[:, 0], ycurv[0, :], edge_order=2)
     Fz = np.ones(zbase.shape)
-    costh = 1/np.sqrt(Fx**2+Fy**2+1)  # Slope angle
-    Fx = -Fx*costh
-    Fy = -Fy*costh
-    Fz = Fz*costh
-    Z = zbase+zchannel*Fz
-    Xmesh = X+zchannel*Fx
-    Ymesh = ycurv+zchannel*Fy
+    costh = 1 / np.sqrt(Fx**2 + Fy**2 + 1)  # Slope angle
+    Fx = -Fx * costh
+    Fy = -Fy * costh
+    Fz = Fz * costh
+    Z = zbase + zchannel * Fz
+    Xmesh = X + zchannel * Fx
+    Ymesh = ycurv + zchannel * Fy
 
     # Reconstruction of regular cartesian mesh
     Xout = np.linspace(Xmesh.min(), Xmesh.max(), nx)
     Xout = np.tile(Xout.reshape((nx, 1)), (1, ny))
     Yout = np.linspace(Ymesh.min(), Ymesh.max(), ny)
     Yout = np.tile(Yout.reshape((1, ny)), (nx, 1))
-    Zout = scipy.interpolate.griddata((Xmesh.reshape(nx*ny),
-                                       Ymesh.reshape(nx*ny)),
-                                      Z.reshape(nx*ny),
-                                      (Xout, Yout), method='cubic')
-    Ztmp = scipy.interpolate.griddata((Xmesh.reshape(nx*ny),
-                                       Ymesh.reshape(nx*ny)),
-                                      Z.reshape(nx*ny),
-                                      (Xout, Yout), method='nearest')
+    Zout = scipy.interpolate.griddata(
+        (Xmesh.reshape(nx * ny), Ymesh.reshape(nx * ny)),
+        Z.reshape(nx * ny),
+        (Xout, Yout),
+        method="cubic",
+    )
+    Ztmp = scipy.interpolate.griddata(
+        (Xmesh.reshape(nx * ny), Ymesh.reshape(nx * ny)),
+        Z.reshape(nx * ny),
+        (Xout, Yout),
+        method="nearest",
+    )
     ind = np.isnan(Zout)
     Zout[ind] = Ztmp[ind]
 
@@ -160,25 +181,43 @@ def gray99(nx=None, ny=None, xmin=-0.4, x1=1.75, x2=2.15,
 
     if plot:
         if theta2 == 0:
-            blod, thin = tilupy.plot.get_contour_intervals(np.nanmin(Zout),
-                                                           np.nanmax(Zout))
+            blod, thin = tilupy.plot.get_contour_intervals(
+                np.nanmin(Zout), np.nanmax(Zout)
+            )
             level_min = thin
         else:
             level_min = None
-        tilupy.plot.plot_topo(Zout.T, Xout[:, 1], Yout[1, :],
-                              level_min=level_min)
+        tilupy.plot.plot_topo(
+            Zout.T, Xout[:, 1], Yout[1, :], level_min=level_min
+        )
 
     return Xout[:, 1], Yout[1, :], Zout.T
 
 
-def channel(nx=None, ny=None, dx=None, dy=None,
-            xmin=-0.4, xmax=3.6, ymax=0.5, xstart_channel=0.65, xend_channel=2.3,
-            xstart_trans=0.4, xend_trans=2.75,
-            R=1.1, bend=0.2, nbends=1,
-            theta_start=40, theta_channel=40, theta_end=0,
-            plot=False, maxh=None, interp_method='linear'):
+def channel(
+    nx=None,
+    ny=None,
+    dx=None,
+    dy=None,
+    xmin=-0.4,
+    xmax=3.6,
+    ymax=0.5,
+    xstart_channel=0.65,
+    xend_channel=2.3,
+    xstart_trans=0.4,
+    xend_trans=2.75,
+    R=1.1,
+    bend=0.2,
+    nbends=1,
+    theta_start=40,
+    theta_channel=40,
+    theta_end=0,
+    plot=False,
+    maxh=None,
+    interp_method="linear",
+):
     """
-    Generate channel with potential multiple bends. Input coordinates are 
+    Generate channel with potential multiple bends. Input coordinates are
     curvilinear along the flattened topography.
 
     Parameters
@@ -239,23 +278,23 @@ def channel(nx=None, ny=None, dx=None, dy=None,
     theta_end = np.deg2rad(theta_end)
 
     if ny is None and dy is None:
-        dy = ymax/100
+        dy = ymax / 100
 
     if nx is None and dx is None:
         if dy is not None:
             dx = dy
         else:
-            raise ValueError('nx or dx must be specified as input')
+            raise ValueError("nx or dx must be specified as input")
 
     # x and y coordinates in the flattened topography
     if nx is None:
-        xtopo = np.arange(xmin, xmax+dx/2, dx)
+        xtopo = np.arange(xmin, xmax + dx / 2, dx)
         nx = len(xtopo)
     else:
         xtopo = np.linspace(xmin, xmax, nx)
 
     if ny is None:
-        ytopo = np.arange(-ymax, ymax+dy/2, dy)
+        ytopo = np.arange(-ymax, ymax + dy / 2, dy)
         ny = len(ytopo)
     else:
         ytopo = np.linspace(-ymax, ymax, ny)
@@ -269,47 +308,58 @@ def channel(nx=None, ny=None, dx=None, dy=None,
     # alpha(x) is 1/2R in the channel, and depends on a transition
     # function in the transition zones
     def trans_function(x, x1, x2):
-        xx = 3*((x-x2)/(x1-x2))**2-2*((x-x2)/(x1-x2))**3
+        xx = 3 * ((x - x2) / (x1 - x2)) ** 2 - 2 * ((x - x2) / (x1 - x2)) ** 3
         return xx
+
     alpha = np.zeros((nx, ny))
     ind = (xtopo > xstart_channel) & (xtopo < xend_channel)
-    alpha[ind] = 1/(2*R)
+    alpha[ind] = 1 / (2 * R)
     ind = (xtopo > xstart_trans) & (xtopo <= xstart_channel)
-    alpha[ind] = 1/(2*R)*trans_function(xtopo[ind],
-                                        xstart_channel, xstart_trans)
+    alpha[ind] = (
+        1 / (2 * R) * trans_function(xtopo[ind], xstart_channel, xstart_trans)
+    )
     ind = (xtopo > xend_channel) & (xtopo <= xend_trans)
-    alpha[ind] = 1/(2*R)*trans_function(xtopo[ind], xend_channel, xend_trans)
+    alpha[ind] = (
+        1 / (2 * R) * trans_function(xtopo[ind], xend_channel, xend_trans)
+    )
 
     # the thalweg is centered on y=0 outside [xstart_channel,xend_channel]. Inbetween,
     # it is given by a cos**2
     def end_bend(x, x1, x2):
-        yy = (bend/2)*(1+np.cos(np.pi*(x-x2)/(x1-x2)))
+        yy = (bend / 2) * (1 + np.cos(np.pi * (x - x2) / (x1 - x2)))
         return yy
 
     def mid_bend(x, x1, x2):
-        yy = bend*np.cos(np.pi*(x-x1)/(x2-x1))
+        yy = bend * np.cos(np.pi * (x - x1) / (x2 - x1))
         return yy
+
     thalweg = np.zeros((nx, ny))
 
     if nbends > 0:
-        step = (xend_channel-xstart_channel)/nbends
+        step = (xend_channel - xstart_channel) / nbends
 
-        ind = (xtopo > xstart_channel) & (xtopo < xstart_channel+step/2)
+        ind = (xtopo > xstart_channel) & (xtopo < xstart_channel + step / 2)
         thalweg[ind] = end_bend(
-            xtopo[ind], xstart_channel, xstart_channel+step/2)
-        ind = (xtopo >= xend_channel-step/2) & (xtopo < xend_channel)
-        thalweg[ind] = (-1)**(nbends+1)*end_bend(xtopo[ind],
-                                                 xend_channel, xend_channel-step/2)
+            xtopo[ind], xstart_channel, xstart_channel + step / 2
+        )
+        ind = (xtopo >= xend_channel - step / 2) & (xtopo < xend_channel)
+        thalweg[ind] = (-1) ** (nbends + 1) * end_bend(
+            xtopo[ind], xend_channel, xend_channel - step / 2
+        )
         if nbends > 1:
-            ind = (xtopo >= xstart_channel+step /
-                   2) & (xtopo < xend_channel-step/2)
+            ind = (xtopo >= xstart_channel + step / 2) & (
+                xtopo < xend_channel - step / 2
+            )
             thalweg[ind] = mid_bend(
-                xtopo[ind], xstart_channel+step/2, xstart_channel+(3/2)*step)
+                xtopo[ind],
+                xstart_channel + step / 2,
+                xstart_channel + (3 / 2) * step,
+            )
 
-    htopo = alpha*(ytopo-thalweg)**2
+    htopo = alpha * (ytopo - thalweg) ** 2
 
     if not maxh:
-        maxh = R/2
+        maxh = R / 2
 
     htopo[htopo > maxh] = maxh
 
@@ -327,63 +377,74 @@ def channel(nx=None, ny=None, dx=None, dy=None,
     slope_angle[ind] = theta_channel
 
     ind = (xtopo >= xstart_trans) & (xtopo < xstart_channel)
-    slope_angle[ind] = (xtopo[ind]-xstart_trans)/(xstart_channel -
-                                                  xstart_trans)
-    slope_angle[ind] = slope_angle[ind]*(theta_channel-theta_start)+theta_start
+    slope_angle[ind] = (xtopo[ind] - xstart_trans) / (
+        xstart_channel - xstart_trans
+    )
+    slope_angle[ind] = (
+        slope_angle[ind] * (theta_channel - theta_start) + theta_start
+    )
 
     ind = (xtopo >= xend_channel) & (xtopo < xend_trans)
-    slope_angle[ind] = (xtopo[ind]-xend_trans)/(xend_channel -
-                                                xend_trans)
-    slope_angle[ind] = slope_angle[ind]*(theta_channel-theta_end)+theta_end
+    slope_angle[ind] = (xtopo[ind] - xend_trans) / (xend_channel - xend_trans)
+    slope_angle[ind] = (
+        slope_angle[ind] * (theta_channel - theta_end) + theta_end
+    )
 
-    bz = scipy.integrate.cumtrapz(-np.sin(slope_angle),
-                                  xtopo, axis=0, initial=0)
-    bz = bz-np.min(bz)
+    bz = scipy.integrate.cumtrapz(
+        -np.sin(slope_angle), xtopo, axis=0, initial=0
+    )
+    bz = bz - np.min(bz)
 
     # Get the coordinates of (xtopo,ytopo) in the cartesian reference frame
     # by=ytopo
     bx = scipy.integrate.cumtrapz(
-        np.cos(slope_angle), xtopo, axis=0, initial=0)
-    bx = bx+xmin*np.cos(theta_start)
+        np.cos(slope_angle), xtopo, axis=0, initial=0
+    )
+    bx = bx + xmin * np.cos(theta_start)
 
     # Vector normal to topography in cartesian coordinates
     # (nx,ny,nz)=(-sin(theta),0,cos(theta))
     # as the topography does vary in the y direction
     # The real topography is thus given in cartesian coordinates by
     # (xcart,ycart,zcart)=(bx,by,bz)+htopo(nx,ny,nz)
-    xcart = bx+htopo*np.sin(slope_angle)
-    zcart = bz+htopo*np.cos(slope_angle)
+    xcart = bx + htopo * np.sin(slope_angle)
+    zcart = bz + htopo * np.cos(slope_angle)
 
     # Reconstruct regular mesh for interpolation
     Xout = np.linspace(xcart[0, 0], xcart[-1, 0], nx)
     Yout = ytopo
     Xout = np.tile(Xout[:, np.newaxis], (1, ny))
-    Zout = scipy.interpolate.griddata((xcart.reshape(nx*ny),
-                                       ytopo.reshape(nx*ny)),
-                                      zcart.reshape(nx*ny),
-                                      (Xout, Yout), method=interp_method)
-    Ztmp = scipy.interpolate.griddata((xcart.reshape(nx*ny),
-                                       ytopo.reshape(nx*ny)),
-                                      zcart.reshape(nx*ny),
-                                      (Xout, Yout), method='nearest')
+    Zout = scipy.interpolate.griddata(
+        (xcart.reshape(nx * ny), ytopo.reshape(nx * ny)),
+        zcart.reshape(nx * ny),
+        (Xout, Yout),
+        method=interp_method,
+    )
+    Ztmp = scipy.interpolate.griddata(
+        (xcart.reshape(nx * ny), ytopo.reshape(nx * ny)),
+        zcart.reshape(nx * ny),
+        (Xout, Yout),
+        method="nearest",
+    )
     ind = np.isnan(Zout)
     Zout[ind] = Ztmp[ind]
 
     if plot:
         if theta_end == 0:
-            blod, thin = tilupy.plot.get_contour_intervals(np.nanmin(Zout),
-                                                           np.nanmax(Zout))
+            blod, thin = tilupy.plot.get_contour_intervals(
+                np.nanmin(Zout), np.nanmax(Zout)
+            )
             level_min = thin
         else:
             level_min = None
-        tilupy.plot.plot_topo(Zout.T, Xout[:, 1], Yout[1, :],
-                              level_min=level_min)
+        tilupy.plot.plot_topo(
+            Zout.T, Xout[:, 1], Yout[1, :], level_min=level_min
+        )
 
     return Xout[:, 1], Yout[1, :], Zout.T, thalweg
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # %% Test gray99
     X, Y, Z = gray99(plot=True)
 
@@ -405,11 +466,20 @@ if __name__ == '__main__':
     theta_start = 10
     theta_channel = 10
     theta_end = 0
-    x, y, z, t = channel(nx, ny, xmin=xmin, xmax=xmax, ymax=ymax,
-                         xstart_channel=xstart_channel,
-                         xend_channel=xend_channel,
-                         xstart_trans=xstart_trans,
-                         theta_start=theta_start,
-                         theta_end=theta_end,
-                         theta_channel=theta_channel,
-                         R=R, bend=bend, maxh=R, plot=True)
+    x, y, z, t = channel(
+        nx,
+        ny,
+        xmin=xmin,
+        xmax=xmax,
+        ymax=ymax,
+        xstart_channel=xstart_channel,
+        xend_channel=xend_channel,
+        xstart_trans=xstart_trans,
+        theta_start=theta_start,
+        theta_end=theta_end,
+        theta_channel=theta_channel,
+        R=R,
+        bend=bend,
+        maxh=R,
+        plot=True,
+    )
