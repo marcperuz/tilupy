fix(read): correct numpy gradient usage
Change the orientation of y axis when calling np.gradient to match
np.gradient indexing convention.

src/tilupy/models/shaltop/read.py
@@ -445,7 +445,7 @@ class Results(tilupy.read.Results):
         file = os.path.join(self.folder_output, "ut" + ".bin")
         ut = read_file_bin(file, self.nx, self.ny)
 
-        [Fx, Fy] = np.gradient(self.zinit, self.y, self.x)
+        [Fx, Fy] = np.gradient(self.zinit, np.flip(self.y), self.x)
         u = u * self.costh[:, :, np.newaxis]
         ut = ut * self.costh[:, :, np.newaxis]
         d = np.sqrt(

src/tilupy/read.py
@@ -762,7 +762,7 @@ class Results:
 
     def get_costh(self):
         """Get cos(slope) of topography"""
-        [Fx, Fy] = np.gradient(self.zinit, self.y, self.x)
+        [Fx, Fy] = np.gradient(self.zinit, np.flip(self.y), self.x)
         costh = 1 / np.sqrt(1 + Fx**2 + Fy**2)
         return costh
 