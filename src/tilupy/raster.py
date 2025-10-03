c4a80629058277b39cfdd64641d226ff2ca91fd1
Boucard  Alexandre <a.boucard@brgm.fr>
2025-10-03 14:47:05 +0200

    docs(raster): add documentation
    Add documentation to all functions.

--- src/tilupy/raster.py
+++ src/tilupy/raster.py
@@ -10,14 +10,38 @@ import numpy as np
 import importlib
 
 
-def read_raster(file):
+def read_raster(file: str) -> list[np.ndarray]:
+    """Convert a raster file (tif or asc) into numpy array.
+
+    Parameters
+    ----------
+    file : str
+        Path to the raster file.
+
+    Returns
+    -------
+    list[np.ndarray]
+        X and Y coordinates and data values in numpy array. 
+    """
     if file.endswith(".asc") or file.endswith(".txt"):
         return read_ascii(file)
     elif file.endswith(".tif") or file.endswith(".tif"):
         return read_tiff(file)
 
 
-def read_tiff(file):
+def read_tiff(file: str) -> list[np.ndarray]:
+    """Read and convert a tiff file into numpy array.
+
+    Parameters
+    ----------
+    file : str
+        Path to the tiff file.
+
+    Returns
+    -------
+    list[np.ndarray]
+        X and Y coordinates and data values in numpy array. 
+    """
     import rasterio
 
     with rasterio.open(file, "r") as src:
@@ -28,19 +52,18 @@ def read_tiff(file):
     return x, y, dem
 
 
-def read_ascii(file):
-    """
-    Read ascii grid file to numpy ndarray.
+def read_ascii(file: str) -> list[np.ndarray]:
+    """Read and convert a ascii file into numpy array.
 
     Parameters
     ----------
-    file : TYPE
-        DESCRIPTION.
+    file : str
+        Path to the ascii file.
 
     Returns
     -------
-    None.
-
+    list[np.ndarray]
+        X and Y coordinates and data values in numpy array. 
     """
     dem = np.loadtxt(file, skiprows=6)
     grid = {}
@@ -63,25 +86,24 @@ def read_ascii(file):
     return x, y, dem
 
 
-def write_tiff(x, y, z, file_out, **kwargs):
-    """
-    Write raster as tif file
+def write_tiff(x: np.ndarray, 
+               y: np.ndarray, 
+               z: np.ndarray, 
+               file_out: str, 
+               **kwargs
+               ) -> None:
+    """Write tif file from numpy array.
 
     Parameters
     ----------
-    x : TYPE
-        DESCRIPTION.
-    y : TYPE
-        DESCRIPTION.
-    z : TYPE
-        DESCRIPTION.
-    file_out : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
+    x : np.ndarray
+        X coordinates.
+    y : np.ndarray
+        Y coordinates.
+    z : np.ndarray
+        Elevation values.
+    file_out : str
+        Name of the output folder.
     """
     import rasterio
     from rasterio.transform import Affine
@@ -89,52 +111,73 @@ def write_tiff(x, y, z, file_out, **kwargs):
     if "driver" not in kwargs:
         kwargs["driver"] = "GTiff"
     res = (x[-1] - x[0]) / (len(x) - 1)
-    transform = Affine.translation(
-        x[0] - res / 2, y[-1] - res / 2
-    ) * Affine.scale(res, -res)
-    with rasterio.open(
-        file_out,
-        "w",
-        height=z.shape[0],
-        width=z.shape[1],
-        count=1,
-        dtype=z.dtype,
-        transform=transform,
-        **kwargs
-    ) as dst:
+    transform = Affine.translation(x[0] - res / 2, y[-1] - res / 2) * Affine.scale(res, -res)
+    
+    with rasterio.open(file_out,
+                       "w",
+                       height=z.shape[0],
+                       width=z.shape[1],
+                       count=1,
+                       dtype=z.dtype,
+                       transform=transform,
+                       **kwargs) as dst:
         dst.write(z, 1)
 
 
-def write_ascii(x, y, z, file_out):
-    """
-    Write raster as ascii file.
+def write_ascii(x: np.ndarray, 
+                y: np.ndarray, 
+                z: np.ndarray, 
+                file_out: str, 
+                ) -> None:
+    """Write ascii file from numpy array.
 
     Parameters
     ----------
-    x : TYPE
-        DESCRIPTION.
-    y : TYPE
-        DESCRIPTION.
-    z : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
+    x : np.ndarray
+        X coordinates.
+    y : np.ndarray
+        Y coordinates.
+    z : np.ndarray
+        Elevation values.
+    file_out : str
+        Name of the output folder.
     """
     nx = z.shape[1]
     ny = z.shape[0]
     cellsize = x[1] - x[0]
-    header_txt = (
-        "ncols {:.0f}\nnrows {:.0f}\nxllcorner {:.5f}\nyllcorner {:.5f}\n"
-    )
+    header_txt = ("ncols {:.0f}\nnrows {:.0f}\nxllcorner {:.5f}\nyllcorner {:.5f}\n")
     header_txt += "cellsize {:.4f}\nnodata_value -99999"
     header_txt = header_txt.format(nx, ny, x[0], y[0], cellsize)
     np.savetxt(file_out, z, header=header_txt, comments="")
 
 
-def write_raster(x, y, z, file_out, fmt=None, **kwargs):
+def write_raster(x: np.ndarray, 
+                 y: np.ndarray, 
+                 z: np.ndarray, 
+                 file_out: str,
+                 fmt: str = None,
+                 **kwargs
+                 ) -> None:
+    """Write raster file from numpy array.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        X coordinates.
+    y : np.ndarray
+        Y coordinates.
+    z : np.ndarray
+        Elevation values.
+    file_out : str
+        Name of the output folder.
+    fmt : str
+        Wanted format : "asc", "ascii", "txt", "tif", "tiff".
+    
+    Raises
+    ------
+    ValueError
+        If invalid format.
+    """
     # File format read from file_out overrides fmt
     fmt_tmp = file_out.split(".")
     if len(fmt_tmp) > 1:
@@ -149,12 +192,8 @@ def write_raster(x, y, z, file_out, fmt=None, **kwargs):
 
     if fmt.startswith("tif"):
         if importlib.util.find_spec("rasterio") is None:
-            print(
-                (
-                    "rasterio is required to write tif files.",
-                    " Switching to asc format",
-                )
-            )
+            print(("rasterio is required to write tif files.",
+                   " Switching to asc format",))
             fmt = "asc"
 
     if fmt in ["asc", "ascii", "txt"]:
